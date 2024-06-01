80016382ed0931511b27e106705b63658c559280
Akshay Raina <akshay9raina@gmail.com>
2024-06-01 18:50:41 +0530

    aas

---
+++ .sfdx/typings/lwc/apex/EventDetailsController.d.ts
@@ -0,0 +1,9 @@
+declare module "@salesforce/apex/EventDetailsController.getSpeakers" {
+  export default function getSpeakers(param: {eventId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/EventDetailsController.getLocationDetails" {
+  export default function getLocationDetails(param: {eventId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/EventDetailsController.getAttendees" {
+  export default function getAttendees(param: {eventId: any}): Promise<any>;
+}
